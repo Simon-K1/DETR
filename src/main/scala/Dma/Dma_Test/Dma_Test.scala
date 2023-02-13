d00fdcc399c30d046d5cbea3fc6175fef106def2
surface book <2507317801@qq.com>
2023-02-13 22:34:41 +0800

    surface book:初步确定Weight_Cache的大体框架

--- src/main/scala/Dma/Dma_Test/Dma_Test.scala
+++
@@ -1,6 +0,0 @@
-package Dma.Dma_Test
-//研究Dma
-import spinal.core._
-class Dma_Test extends Component{
-    
-}
--- src/main/scala/Systolic_Array/Weight_Cache.scala
+++ src/main/scala/Systolic_Array/Weight_Cache.scala
@@ -5,6 +5,19 @@ import spinal.lib.slave
 import utils._
 import xip.xil_SimpleDualBram
 //实现权重矩阵的缓存与输出计算
+//初步思路:在所有计算开始前应该先缓存所有权重,
+    //如果片上资源不够,应该进行矩阵切块,这里将权重矩阵切4块,也就是需要调用计算模块4次
+    //---->768/4=192,也就是每次启动只能算192个输出通道,
+    //192/8=24,激活值循环24次,也可以认为是每个Weight_Bram存24个16*16*8的卷积核,每个Bram应该存储16*16*8*24(12个Bram)个字节
+object WEIGHT_CACHE_STATUS extends SpinalEnum(defaultEncoding = binaryOneHot){
+    //权重缓存模块的状态
+    val IDLE, INIT, SEND_WEIGHT= newElement
+    //感觉权重模块很简单,给一个启动信号即可一直循环出数
+    //唯一的要求就是出去的权重必须和Img2Col后的图片对上
+}
+
+
+
 class Weight_Cache extends Component{
     //应该创建8列缓存单元,卷积核循环填充到这8个缓存单元中
     val Config=TopConfig()
@@ -17,7 +30,7 @@ class Weight_Cache extends Component{
     //构建8列权重缓存
     val Weight_Cache=Array.tabulate(Config.SA_COL){
         i=>def gen()={
-            val Weight_Bram=new xil_SimpleDualBram(64,512*20,8,"Weight_Bram",i==0)
+            val Weight_Bram=new xil_SimpleDualBram(64,6144+5,8,"Weight_Bram",i==0)
             Weight_Bram.io.addra:=0
             Weight_Bram.io.addrb:=0
             // Weight_Bram.io.doutb:=0
