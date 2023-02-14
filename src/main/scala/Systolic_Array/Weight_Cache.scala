99058f3e64421310151b032250493f67753b419b
roucedupon <2507317801@qq.com>
2023-02-14 17:35:47 +0800

    权重缓存模块写了一个状态机，提交一把

--- src/main/scala/Systolic_Array/Weight_Cache.scala
+++ src/main/scala/Systolic_Array/Weight_Cache.scala
@@ -4,6 +4,7 @@ import spinal.core._
 import spinal.lib.slave
 import utils._
 import xip.xil_SimpleDualBram
+import spinal.core
 //实现权重矩阵的缓存与输出计算
 //初步思路:在所有计算开始前应该先缓存所有权重,
     //如果片上资源不够,应该进行矩阵切块,这里将权重矩阵切4块,也就是需要调用计算模块4次
@@ -15,17 +16,55 @@ object WEIGHT_CACHE_STATUS extends SpinalEnum(defaultEncoding = binaryOneHot){
     //感觉权重模块很简单,给一个启动信号即可一直循环出数
     //唯一的要求就是出去的权重必须和Img2Col后的图片对上
 }
+case class WeightCache_Fsm(start:Bool)extends Area{
+    val currentState = Reg(WEIGHT_CACHE_STATUS()) init WEIGHT_CACHE_STATUS.IDLE
+    val nextState = WEIGHT_CACHE_STATUS()
+    currentState := nextState
 
+    val Init_End=Bool()
+    val Weight_All_Sended=Bool()
+    switch(currentState){
+        is(WEIGHT_CACHE_STATUS.IDLE){
+            when(start){
+                nextState:=WEIGHT_CACHE_STATUS.INIT
+            }otherwise{
+                nextState:=WEIGHT_CACHE_STATUS.IDLE
+            }
+        }
+        is(WEIGHT_CACHE_STATUS.INIT){
+            when(Init_End){
+                nextState:=WEIGHT_CACHE_STATUS.SEND_WEIGHT
+            }otherwise{
+                nextState:=WEIGHT_CACHE_STATUS.INIT
+            }
+        }
+        is(WEIGHT_CACHE_STATUS.SEND_WEIGHT){
+            when(Weight_All_Sended){
+                nextState:=WEIGHT_CACHE_STATUS.IDLE
+            }otherwise{
+                nextState:=WEIGHT_CACHE_STATUS.SEND_WEIGHT
+            }
+        }
+    }
+}
 
 
 class Weight_Cache extends Component{
     //应该创建8列缓存单元,卷积核循环填充到这8个缓存单元中
     val Config=TopConfig()
     val io=new Bundle{
+        val start=in Bool()
         val sData=slave Stream(UInt(64 bits))
-
+        val Matrix_Row=in UInt(16 bits)//先初步默认16bit，以后不够再改
+        val Matrix_Col=in UInt(16 bits)//每次计算的权重矩阵的实际列数（对应输出通道数量）
+                                        //比如输出通道是768，但是资源不够，所以只能存192个输出通道，所以这里输入192
+        
     }
     noIoPrefix()
+    val Fsm=WeightCache_Fsm(io.start)
+    val Init_Count=WaCounter(Fsm.currentState===WEIGHT_CACHE_STATUS.INIT,3,5)//数5下进行初始化
+    Fsm.Init_End:=Init_Count.valid
+
 
     //构建8列权重缓存
     val Weight_Cache=Array.tabulate(Config.SA_COL){
