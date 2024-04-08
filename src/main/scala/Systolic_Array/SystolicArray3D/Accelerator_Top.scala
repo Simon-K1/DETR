cbb23ce93bb580394fd976a943de961953f507e8
Simon-K1 <1223987490@qq.com>
2024-04-08 10:31:53 +0800

    删掉SA_3D_TOP

--- src/main/scala/Systolic_Array/SystolicArray3D/Accelerator_Top.scala
+++ src/main/scala/Systolic_Array/SystolicArray3D/Accelerator_Top.scala
@@ -12,64 +12,6 @@ import xip.xil_ila
 import utils.TopConfig
 import utils.Tcl_Config
 
-class Accelerator_Top extends Component{//此版本已失效（此版本用的是一个大状态机做全局控制，现在已经不那样用了）
-  val Config=TopConfig()
-  val regSData = slave(AxiLite4(log2Up(1 MiB), 32))//地址位宽-数据位宽
-  AxiLite4SpecRenamer(regSData)
-  val Regs=new RegTable
-  val core=new SA3D_Top(1,8,64,32)
-  val s_axis_s2mm=new Bundle{
-    val Data_Width=64
-    val tdata=in UInt(Data_Width bits)
-    val tkeep=in Bits(Data_Width/8 bits)
-    val tlast=in Bool()
-    val tready=out Bool()
-    val tvalid=in Bool()
-  }
-  val m_axis_mm2s=new Bundle{
-    val Data_Width=64
-    val tdata=out UInt(Data_Width bits)
-    val tkeep=out Bits(Data_Width/8 bits)
-    val tlast=out Bool()
-    val tready=in Bool()
-    val tvalid=out Bool()
-  }
-  Regs.regSData<>regSData
-  core.s_axis_s2mm<>s_axis_s2mm
-  core.m_axis_mm2s<>m_axis_mm2s
-
-  core.Control.start                                  :=Regs.Start
-  core.Control.Switch_Conv                            :=Regs.SwitchConv
-  // core.Control
-    
-  core.Img2Col_Instru.Stride                          :=Regs.Stride.resized                        
-  core.Img2Col_Instru.Kernel_Size                     :=Regs.Kernel_Size.resized                   
-  core.Img2Col_Instru.Window_Size                     :=Regs.Window_Size.resized                   
-  core.Img2Col_Instru.InFeature_Size                  :=Regs.InFeature_Size.resized                
-  core.Img2Col_Instru.InFeature_Channel               :=Regs.InFeature_Channel.resized             
-  core.Img2Col_Instru.OutFeature_Channel              :=Regs.OutFeature_Channel.resized //输出图片通道也可以是权重矩阵的列数           
-  core.Img2Col_Instru.OutFeature_Size                 :=Regs.OutFeature_Size.resized               
-  core.Img2Col_Instru.Sliding_Size                    :=Regs.Sliding_Size.resized                  
-  core.Img2Col_Instru.OutCol_Count_Times              :=Regs.OutCol_Count_Times.resized            
-  core.Img2Col_Instru.InCol_Count_Times               :=Regs.InCol_Count_Times.resized             
-  core.Img2Col_Instru.OutRow_Count_Times              :=Regs.OutRow_Count_Times.resized            
-  core.Img2Col_Instru.OutFeature_Channel_Count_Times  :=Regs.OutFeature_Channel_Count_Times.resized
-    
-
-  core.Img2Col_Instru.WeightMatrix_Row                :=Regs.WeightMatrix_Row
-
-  core.Img2Col_Instru.OutMatrix_Col                   :=Regs.OutMatrix_Col
-  core.Img2Col_Instru.OutMatrix_Row                   :=Regs.OutMatrix_Row//输出矩阵的行数
-
-//   core.GemmInstru.HEIGHT                              :=Regs.Gemm_Height
-//   core.GemmInstru.WIDTH                               :=Regs.Gemm_Width
-
-    core.QuantInstru.zeroIn                             :=Regs.Quant_ZeroPoint
-
-
-  }
-
-
 
 class Accelerator_TopV2(SLICE:Int,HEIGHT:Int,WIDTH:Int,ACCU_WITDH:Int,val MODULE_NUM:Int=5,WW_Detpth:Int=1024) extends Component{
   //WW_Detpth 代表的是权重缓存模块Weight cache 的write depth 
