eb41d170c719968ad37e7cf0002ef0f6402eb511
Simon-K1 <1223987490@qq.com>
2024-04-12 15:11:05 +0800

    matlab 代码

--- main/Step1_img2Col_Input_TxtGen.m
+++ main/Step1_img2Col_Input_TxtGen.m
@@ -2,7 +2,7 @@
     % 图片大小，步长等相关信息需要手动配置
 clear
 
-ProjDir="E:\Transformer\Matlab\main\Tests\SA_3D\SA_1_8_8\Padding";%修改
+ProjDir="E:\FPGA\Transformer\MATLAB\Matlab_Main\Transformer_Main\main\DETR";%修改
 PytorchPath="E:\Transformer\Transformer_Arithmatic\Transformer_Main\BinPath\"
 if exist(ProjDir,'dir')
     warning("文件夹已经存在！！")
@@ -11,28 +11,28 @@ else
     mkdir(ProjDir)
     cd(ProjDir)
 end
-FromPytorch=1;%是否采用pytorch生成的真实数据
+FromPytorch=0;%是否采用pytorch生成的真实数据
 Drop_Message=1;%决定卷积完后图片大小要不要变化
-                %默认为1，0选项在加入padding模块后已经没啥用了
+               %默认为1，0选项在加入padding模块后已经没啥用了
 
 
 WEIGHT_VERSION=1;%权重缓存模块的版本，可选1（V1），2（V2，已失效）
-Feature_Size=32;%图片大小224*224（未Padding的图片大小）
+Feature_Size=224;%图片大小224*224（未Padding的图片大小）
 Feature_Channel=8;%图片通道
-Out_Channel=32;%输出图片通道，要修改
+Out_Channel=64;%输出图片通道，要修改
 BinFile_Channel=Out_Channel;%一般来说binfile_channel和Out_Channel一样
 
 enPadding=1;%要修改--要不要padding
-zeroNum=1;%要修改--Padding补零的圈数，如果是1的话，图片的行和高都会加2
-zeroData=123;%要修改，根据Pytorch量化结果来吧，没真实数据就用随机数
+zeroNum=3;%要修改--Padding补零的圈数，如果是1的话，图片的行和高都会加2
+zeroData=0;%要修改，根据Pytorch量化结果来吧，没真实数据就用随机数
 
-Stride=1;%要修改
-KernelSize=3;%要修改
+Stride=2;%要修改
+KernelSize=7;%要修改
 assert(Stride<=KernelSize,"Stride must be less than kernelSize");
 %脉动阵列配置
-Slice=3;
+Slice=1;
 Height=8;%不要改
-Width=64;
+Width=8;
 HeadNums=12;%注意力头的数量，记得修改HeadNums，Tiny=3,Small=6,base=12
 
 %Compute_InChannel=Height;%每次计算的输出通道，
@@ -40,65 +40,52 @@ HeadNums=12;%注意力头的数量，记得修改HeadNums，Tiny=3,Small=6,base=
 
 
 %-----------------------------下面的都不要改-----------------------------------------
-if enPadding%如果要Padding，重新计算Padding后图片的大小
-    Feature_Size=Feature_Size+zeroNum*2
+if enPadding==1 %Feature_Size=Feature_Size+zeroNum*2
+  padding=zeroNum;
+else
+  padding=0;
 end
 
 OutFeatureSize=75;%无需修改，在后面自动推理出来
 assert(mod(Feature_Channel,8)==0,"输入通道必须是8的倍数");
 assert(mod(Out_Channel,8)==0,"输出通道必须是8的倍数");
-%Strdie1
 if Stride==KernelSize
-%     OutFeatureSize=Feature_Size/KernelSize;
     if(mod(Feature_Size,KernelSize)==0)
         OutFeatureSize=Feature_Size/KernelSize;
     else
-%         fprintf("Error:kernelSize必须和FeatureSize匹配\n")
         error('Error:kernelSize必须和FeatureSize匹配\n')
     end
-else 
-    if Stride==1
-    OutFeatureSize=size(1:Feature_Size-KernelSize+1,2);
-
-    else 
-        Tmp=1:Stride:Feature_Size;
-        if ~Drop_Message%若不丢失信息，继续补零
-            
-            if(Tmp(end)~=Feature_Size)
-                Feature_Size=Tmp(end)+KernelSize-1%修改图片大小
-            end
-        else%若丢失信息，则修改输出图片大小
-            %说明：比如3*3卷积，步长为2，输入图片322*322，
-                %对于3*3的步长为2的卷积来说是无法覆盖全部图片的（321*321->160*160,323*323->161*161)
-                %这里就是将322*322的图片变成321*321的图片，得到160*160的输出。
-                %上面就是将322*322的图片变成323*323的图片，得到161*161的输出。
-            %do nothing
-        end
-        OutFeatureSize=size(Tmp,2)-1
-    end
+else
+    OutFeatureSize =floor((Feature_Size+padding*2-KernelSize)/Stride)+1;
 end
-
-
 
 Out_Col=OutFeatureSize;
 Out_Row=OutFeatureSize;
 Out_Col_Lefted=Out_Col;
+
+
 In_Col=Feature_Size;
 In_Row=Feature_Size;
 
-
-Feature_In=zeros(Feature_Size,Feature_Size*Feature_Channel);
+Feature_Size_padding=Feature_Size+padding*2;
+Feature_In=zeros(Feature_Size_padding,Feature_Size_padding*Feature_Channel);
 Feature_Row=zeros(1,Feature_Size);
 
 
 
-for row=1:Feature_Size
-    Row_Data=rand(1,Feature_Size*Feature_Channel)*5;
-    Row_Data_Outer=(Row_Data>255)*255;%将大于255的数变成255
-    Row_Data_Inner=Row_Data.*(Row_Data<=255);%将小于255的数保留
-    Row_Data=Row_Data_Outer+Row_Data_Inner;
-    Row_Data=round(Row_Data);
-    Feature_In(row,:)=Row_Data;
+for row=1:Feature_Size_padding
+    for col=1:Feature_Size_padding*Feature_Channel%注意列数包含通道 每个row都是特征图的一层（包含通道）
+        if row <=padding || row >Feature_Size_padding-padding
+            Row_Data =zeroData;
+            Feature_In(row,col)=Row_Data;
+        elseif (padding < row && row <= Feature_Size_padding - padding) && (col <= padding*Feature_Channel || ((Feature_Size_padding - padding)*Feature_Channel < col ))
+            Row_Data =zeroData;
+            Feature_In(row,col)=Row_Data;
+        else
+            Row_Data=randi([1,255]);
+            Feature_In(row,col)=Row_Data;%Feature_Size*Feature_Channel个Row_Data
+        end
+    end
 end
 
 %% 开始构建权重数据
@@ -118,10 +105,10 @@ save('matlab')
 %% 生成txt文件
 if ~FromPytorch
     fid_raw_W=fopen('Rand_ImageIn.txt','w');
-    Matrix_Flattened=reshape(Feature_In',1,[]);%展平
-    Shape=size(Matrix_Flattened);
-    
-     for i=8:8:Shape(2)
+    Matrix_Flattened=reshape(Feature_In',1,[]);%转置 展平成一行
+    Shape=size(Matrix_Flattened);%取行数 列数
+    %fprintf("shape2 %d\n ",Shape(2))
+     for i=8:8:Shape(2)% 行数*列数(展平了) 一个是8bit
       %通道按从低往高排列，比如第一个通道的点放最低位
       fprintf(fid_raw_W,'%02x%02x%02x%02x%02x%02x%02x%02x\n',Matrix_Flattened(i),Matrix_Flattened(i-1),Matrix_Flattened(i-2),Matrix_Flattened(i-3),Matrix_Flattened(i-4),Matrix_Flattened(i-5),Matrix_Flattened(i-6),Matrix_Flattened(i-7));%低位第一个点，高位第二个点，
      end
