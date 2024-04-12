4dd1ccba6fd6f178bf72fa15dec93916e68fc1e7
roucedupon <2507317801@qq.com>
2024-04-12 22:09:58 +0800

    哎，回退到之前跑通的那一版去

--- main/Step1_img2Col_Input_TxtGen.m
+++ main/Step1_img2Col_Input_TxtGen.m
@@ -2,7 +2,7 @@
     % 图片大小，步长等相关信息需要手动配置
 clear
 
-ProjDir="E:\FPGA\Transformer\MATLAB\Matlab_Main\Transformer_Main\main\DETR";%修改
+ProjDir="E:\Transformer\Matlab\main\Tests\SA_3D\SA_1_8_8\Padding";%修改
 PytorchPath="E:\Transformer\Transformer_Arithmatic\Transformer_Main\BinPath\"
 if exist(ProjDir,'dir')
     warning("文件夹已经存在！！")
@@ -73,21 +73,22 @@ Feature_Row=zeros(1,Feature_Size);
 
 
 
-for row=1:Feature_Size_padding
-    for col=1:Feature_Size_padding*Feature_Channel%注意列数包含通道 每个row都是特征图的一层（包含通道）
-        if row <=padding || row >Feature_Size_padding-padding
-            Row_Data =zeroData;
-            Feature_In(row,col)=Row_Data;
-        elseif (padding < row && row <= Feature_Size_padding - padding) && (col <= padding*Feature_Channel || ((Feature_Size_padding - padding)*Feature_Channel < col ))
-            Row_Data =zeroData;
-            Feature_In(row,col)=Row_Data;
-        else
-            Row_Data=randi([1,255]);
-            Feature_In(row,col)=Row_Data;%Feature_Size*Feature_Channel个Row_Data
-        end
-    end
-end
-
+% for row=1:Feature_Size_padding
+%     for col=1:Feature_Size_padding*Feature_Channel%注意列数包含通道 每个row都是特征图的一层（包含通道）
+%         if row <=padding || row >Feature_Size_padding-padding
+%             Row_Data =zeroData;
+%             Feature_In(row,col)=Row_Data;
+%         elseif (padding < row && row <= Feature_Size_padding - padding) && (col <= padding*Feature_Channel || ((Feature_Size_padding - padding)*Feature_Channel < col ))
+%             Row_Data =zeroData;
+%             Feature_In(row,col)=Row_Data;
+%         else
+%             Row_Data=randi([1,255]);
+%             Feature_In(row,col)=Row_Data;%Feature_Size*Feature_Channel个Row_Data
+%         end
+%     end
+% end
+Feature_In=reshape(randi([0,255],[Feature_Size,Feature_Size,Feature_Channel]),Feature_Size,[])
+Feature_No_Padding=Feature_In(zeroNum+1:end-zeroNum,zeroNum+1:end-zeroNum);
 %% 开始构建权重数据
 WeightMatrix=zeros(Feature_Channel*KernelSize*KernelSize,Out_Channel);%%构建权重矩阵
 WeightMatrix_Row=zeros(1,Out_Channel);
@@ -105,7 +106,7 @@ save('matlab')
 %% 生成txt文件
 if ~FromPytorch
     fid_raw_W=fopen('Rand_ImageIn.txt','w');
-    Matrix_Flattened=reshape(Feature_In',1,[]);%转置 展平成一行
+    Matrix_Flattened=reshape(Feature_No_Padding',1,[]);%转置 展平成一行,注意生成的是原始图片（未padding的）
     Shape=size(Matrix_Flattened);%取行数 列数
     %fprintf("shape2 %d\n ",Shape(2))
      for i=8:8:Shape(2)% 行数*列数(展平了) 一个是8bit
@@ -184,3 +185,8 @@ if FromPytorch
     fclose(fid);
     save("matlab.mat")
 end
+
+
+
+
+
